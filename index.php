eeca384cbcf0bed5011602a3057c9772f150f616
Luli Lowenstein <llowenstein@my.bcit.ca>
2016-11-27 16:15:50 -0800

    first commit to test

---
+++ index.php
@@ -0,0 +1,14 @@
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<div class="container">
+    <div class="jumbotron">
+        <h1>Math Game</h1>
+    </div>
+</div>
+
+<?php
+
+
+?>
